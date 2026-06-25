d52d8efd92501cca705b33b871fa89029ebf1ac7
Dantore <snover9000@gmail.com>
2026-06-25 18:01:50 +0300

    Update libraries.bat

--- libraries.bat
+++ libraries.bat
@@ -1,6 +1,6 @@
 @echo off
 chcp 65001 > nul
-echo [STT] Начинаю автоматическую установку библиотек... 
-pip install faster-whisper pyperclip keyboard
+echo [STT] Идёт установка библиотек... 
+pip install faster-whisper pyperclip keyboard sounddevice numpy
 echo [STT] Установка успешно завершена!
 pause
